82fbe1b0e92ca735eae9c930e42d3748a17048be
MashiKimura <kandai303.m.k@gmail.com>
2022-01-11 15:25:17 +0900

    Userバリデーション変更
    passwordの英数字を含むバリデーション。
    トップページにnicknameの表示。
    User画面遷移の制限。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
     validates :nickname
     validates :birth_day
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 end
