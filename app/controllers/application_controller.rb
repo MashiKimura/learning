f37b8acb56fbbb3e997cb91e8e829b63f9e21b63
MashiKimura <kandai303.m.k@gmail.com>
2022-01-12 11:25:58 +0900

    BASIC認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_action
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :nickname, :birth_day])
+  end
+
+  def basic_action 
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
