b0e594fa2ef98145e3c4d829459cd9e076681b0d
MashiKimura <kandai303.m.k@gmail.com>
2022-01-18 11:46:55 +0900

    コメント削除　編集

--- app/controllers/textbooks_controller.rb
+++ app/controllers/textbooks_controller.rb
@@ -18,37 +18,13 @@ class TextbooksController < ApplicationController
     #学習時間の羅列
     @records = Record.where(textbook_id: @textbook.id)
     sum_time(@records)
-
-    
-    
-    # array = []
-    # @records.each do |record|
-    #   #配列宣言　日付毎の集計
-    #   g_records = @records.where(r_date: record.r_date)
-    #   g_date = ""
-    #   sum_hours = 0 
-    #   sum_minutes = 0
-    #   #日付毎に時間と分を集計
-    #   g_records.each do |g_record|
-    #     g_date = g_record.r_date
-    #     sum_hours += g_record.hours
-    #     sum_minutes += g_record.minutes
-    #   end
-    #   array << {r_date: g_date, hours: sum_hours, minutes: sum_minutes }
-    # end
     #日付でグループ化
     @date_group_h = @records.group_by_day(:r_date).sum(:hours)
     @date_group_m = @records.group_by_day(:r_date).sum(:minutes)
-    # @date_array = []
     @date_hash = {}
     @date_group_h.each do |date_record_h|
       date_m = @date_group_m[date_record_h[0]]
-      # date_hash = {}
-      # date_hash[:date] = date_record_h[0]
       hours = date_m / 60
-      # date_hash[:hours] = date_record_h[1] + hours
-      # date_hash[:minutes] = date_m % 60
-      # @date_array << date_hash
       @date_hash[date_record_h[0]] = date_record_h[1] + hours
     end
     
@@ -65,7 +41,7 @@ class TextbooksController < ApplicationController
     params.require(:textbook).permit(:book, :s_page, :e_page).merge(user_id: current_user.id)
   end
 
-  def sum_time(records) #学習時間の合計 合計を使用しなければ必要なし
+  def sum_time(records)
     @sum_hours = 0
     @sum_minutes = 0
     records.each do |record|
