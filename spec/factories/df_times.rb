cb78187c4fa2d0b459bdf4ac28c6ec9569f8d85b
MashiKimura <kandai303.m.k@gmail.com>
2022-02-02 16:53:17 +0900

    目標管理機能　単体テストコード作成

--- spec/factories/df_times.rb
+++ spec/factories/df_times.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :df_time do
-    
+    d_sun       {Faker::Number.between(from: 0, to: 23)}
+    d_mon       {Faker::Number.between(from: 0, to: 23)}
+    d_tue       {Faker::Number.between(from: 0, to: 23)}
+    d_wed       {Faker::Number.between(from: 0, to: 23)}
+    d_thu       {Faker::Number.between(from: 0, to: 23)}
+    d_fri       {Faker::Number.between(from: 0, to: 23)}
+    d_sat       {Faker::Number.between(from: 0, to: 23)}
+    association :textbook
   end
 end
--- spec/models/df_time_spec.rb
+++ spec/models/df_time_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe DfTime, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @df_time = FactoryBot.build(:df_time)
+  end
+
+  describe '目標学習時間の登録' do
+    context '目標学習時間が登録できるとき' do
+      it '全ての項目が入力されていれば登録できる' do
+        @df_time.valid?
+        expect(@df_time).to be_valid
+      end
+    end
+    context '目標学習時間が登録できないとき' do
+      it 'いずれかの項目が空白の場合は登録できない' do
+        @df_time.d_mon = ""
+        @df_time.valid?
+        expect(@df_time.errors.full_messages).to include("D mon can't be blank")
+      end
+      it 'いずれかの項目が0より少ない場合は登録できない' do
+        @df_time.d_mon = -3
+        @df_time.valid?
+        expect(@df_time.errors.full_messages).to include("D mon must be greater than or equal to 0")
+      end
+      it 'いずれかの項目が23より大きい場合は登録できない' do
+        @df_time.d_mon = 24
+        @df_time.valid?
+        expect(@df_time.errors.full_messages).to include("D mon must be less than or equal to 23")
+      end
+      it 'いずれかの項目が小数の場合は登録できない' do
+        @df_time.d_mon = 10.1
+        @df_time.valid?
+        expect(@df_time.errors.full_messages).to include("D mon must be an integer")
+      end
+    end
+  end
 end
